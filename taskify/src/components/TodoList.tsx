1346f1c01dd1bdacc9a58f1596624ab9f6b7b586
kukis00 <kubik54321@gmail.com>
2023-04-30 16:32:33 +0200

    Working on todos

---
+++ taskify/src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+import { Todo } from '../model'
+
+interface Props {
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const TodoList:React.FC<Props> = ({todos, setTodos}) => {
+  return (
+    <div className='todos'>
+        {
+            todos.map((todo) => (
+                <li>{todo.todo}</li>
+            ))
+        }
+    </div>
+  )
+}
+
+export default TodoList
